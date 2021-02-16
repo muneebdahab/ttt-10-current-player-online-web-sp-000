cce65758d8df4be3a6d6be6f6da05e5cbc71784f
Muneeb Dahab <muneeb.dahab@outlook.com>
2021-02-16 16:28:55 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -32,4 +32,16 @@ def position_taken?(board, index)
    end
 end
 
+def turn_count(board)
+   counter = 0
+   board.each do |spaces|
+      if spaces == "X" || spaces == "O"
+         counter += 1
+      end
+   end
+   counter
+end
+def current_player(board)
+   turn_count(board) % 2 == 0 ? "X" : "O"
+end
 
